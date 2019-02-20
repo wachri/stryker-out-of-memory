731e3f595e16c24aa7e55a12c801b03999ccf48c
Christian Waldmann <christian.waldmann@hetzner-cloud.de>
2019-02-20 13:20:26 +0100

    Add stryker

---
+++ stryker.conf.js
@@ -0,0 +1,19 @@
+module.exports = function(config) {
+    config.set({
+        mutator: 'typescript',
+        packageManager: 'yarn',
+        reporters: ['clear-text', 'progress'],
+        testRunner: 'jest',
+        transpilers: [],
+        logLevel: 'trace',
+        coverageAnalysis: 'off',
+        mutate: [
+            'src/app/*.ts',
+            '!src/**/*.spec.ts',
+            '!src/**/*.mock.ts',
+            '!**/*.d.ts',
+            '!**/*.snap',
+            '!**/*.module.ts'
+        ],
+    });
+};
